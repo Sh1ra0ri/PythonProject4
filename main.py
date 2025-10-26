0be8183dd211266aacb781b94ccca70647d438b3
Sh1ra0ri <xxnon.7@gmail.com>
2025-10-27 00:47:28 +0300

    .

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ HTML_URL = "https://raw.githubusercontent.com/Sh1ra0ri/PythonProject4/main/conta
 HTML = requests.get(HTML_URL).text
 
 class Handler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):  # ← ОБЯЗАТЕЛЬНО do_GET (с большой G и E)
+    def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/html; charset=utf-8")
         self.end_headers()
